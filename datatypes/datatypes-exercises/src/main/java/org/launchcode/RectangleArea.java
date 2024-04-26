a2b1f289e0a807c84833f1f2499daa8873e24181
ohcrums <149456208+ohcrums@users.noreply.github.com>
2024-04-25 19:25:22 -0500

    ch 2

---
+++ datatypes/datatypes-exercises/src/main/java/org/launchcode/RectangleArea.java
@@ -0,0 +1,20 @@
+package org.launchcode;
+
+import java.util.Scanner;
+
+public class NumericTypes {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("I will calculate the area of a rectangle.\nWhat is the length of the rectangle? (Whole numbers and decimal numbers only.)");
+        double length = input.nextDouble();
+
+        System.out.println("What is the width of the rectangle? (Whole numbers and decimal numbers only.)");
+        double width = input.nextDouble();
+
+        double area;
+        area = width * length;
+
+        System.out.println("The area of the rectangle is " + area + " units of whatever metric you provided. Didn't ask.");
+
+    }
+}
---
+++ datatypes/datatypes-exercises/src/main/java/org/launchcode/WonderlandMadlibs.java
@@ -0,0 +1,34 @@
+package org.launchcode;
+
+import java.util.Scanner;
+
+public class StringSearch {
+    public static void main(String[] args) {
+        String wonderQuote = "Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do: once or twice she had peeped into the book her sister was reading, but it had no pictures or conversations in it, ‘and what is the use of a book,’ thought Alice ‘without pictures or conversation?’";
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a search term:");
+
+        String searchTerm = input.nextLine();
+
+        boolean searchResult = wonderQuote.toLowerCase().contains( searchTerm.toLowerCase() );
+
+        int resultIndex = wonderQuote.indexOf(searchTerm);
+        int searchLength = searchTerm.length();
+
+        System.out.println("Your search term is " + searchLength + " characters long.");
+
+        if (!searchResult) {
+            System.out.println("Search term '" + searchTerm + "' not found :(");
+        } else {
+            System.out.println("Search term '" + searchTerm + "' was found :D" +
+                    "\nIt first appears at character-index " + resultIndex + "." +
+                    "\nWhat would you like to replace the word '" + searchTerm + "' with?");
+
+            String newWord = input.nextLine().trim();
+            String crazySentence = wonderQuote.replace(searchTerm, newWord);
+            System.out.println(crazySentence + "\n---\nWasn't that fun? Don't answer.");
+        }
+        input.close();
+    }
+}
