6a3601a48dd1cc577749570b3c8622fdbb4d60a2
ohcrums <149456208+ohcrums@users.noreply.github.com>
2024-04-29 15:41:43 -0500

    done

---
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -0,0 +1,29 @@
+package org.launchcode;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class Area {
+    // prompts user for the radius of a circle, calculate its area, print the result.
+    // A = pi * r * r
+    public static void main (String[] args) {
+        double radius;
+
+        Scanner input;
+        input = new Scanner(System.in);
+
+        System.out.println("I will calculate the area of a circle...\n" +
+                "Enter the radius of a circle:");
+
+        radius = input.nextDouble();
+        input.close();
+
+        double area = Circle.getArea(radius);
+
+        System.out.println("The area of a circle whose radius is " + radius + ", is equal to " + area);
+        System.out.println("""
+                ***
+                Disclaimer:
+                Please note that this calculation uses Pi only to the hundredth decimal point, ignoring the following decimals :)""");
+    }
+}
---
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Circle.java
@@ -0,0 +1,8 @@
+package org.launchcode;
+
+public class Circle {
+    public static Double getArea(Double radius) {
+        double pi = 3.14;
+        return pi * radius * radius;
+    }
+}
