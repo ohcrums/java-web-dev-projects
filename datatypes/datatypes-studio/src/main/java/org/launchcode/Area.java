23823af4e1f3e9d6fd35cca4c101bab9d3613569
ohcrums <149456208+ohcrums@users.noreply.github.com>
2024-04-29 17:01:17 -0500

    input validation

--- datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -12,16 +12,30 @@ public class Area {
         input = new Scanner(System.in);
 
         System.out.println("I will calculate the area of a circle...\n" +
-                "Enter the radius of a circle:");
+                "Enter the radius of a circle, integers only:");
 
-        radius = input.nextDouble();
+        do {
+            if (input.hasNextDouble()) {
+                radius = input.nextDouble();
+                if (radius > 0) {
+                    break;
+                } else {
+                    System.out.println("Enter a number above 0...");
+                }
+            } else {
+                System.out.println("It needs to be a *number* above 0...");
+                input.next();
+            }
+        } while (true);
         input.close();
 
         double area = Circle.getArea(radius);
 
-        System.out.println("The area of a circle whose radius is " + radius + ", is equal to " + area);
+        System.out.println("Great!\n" +
+                "*******\n" +
+                "The area of a circle whose radius is " + radius + ", is equal to " + area);
         System.out.println("""
-                ***
+                *******
                 Disclaimer:
                 Please note that this calculation uses Pi only to the hundredth decimal point, ignoring the following decimals :)""");
     }
