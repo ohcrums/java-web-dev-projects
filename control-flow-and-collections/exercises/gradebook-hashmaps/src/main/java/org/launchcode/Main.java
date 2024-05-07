a20a60d4ce8ea7a72723e95c174f8e46897dbfac
ohcrums <149456208+ohcrums@users.noreply.github.com>
2024-05-06 21:16:43 -0500

    done

---
+++ control-flow-and-collections/exercises/gradebook-hashmaps/src/main/java/org/launchcode/Main.java
@@ -0,0 +1,39 @@
+package org.launchcode;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        String newStudent;
+        HashMap<Integer, String> classRoster = new HashMap<>();
+
+        do {
+            System.out.print("Student: ");
+            newStudent = input.nextLine();
+
+            if (!newStudent.equals("")) {
+                System.out.print("ID: ");
+                Integer newID = input.nextInt();
+                classRoster.put(newID, newStudent);
+
+                input.nextLine();
+            }
+        } while(!newStudent.equals(""));
+
+        input.close();
+
+        System.out.println("\nClass roster:");
+
+        for (Map.Entry<Integer, String> student : classRoster.entrySet()) {
+            System.out.println(student.getValue() + "'s ID: " + student.getKey());
+        }
+
+        System.out.println("Number of students in roster: " + classRoster.size());
+
+    }
+}
