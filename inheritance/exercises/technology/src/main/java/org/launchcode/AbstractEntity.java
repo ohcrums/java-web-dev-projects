b03ca24b1d1c04e0e6420bb76212fadd23ff79c1
ohcrums <149456208+ohcrums@users.noreply.github.com>
2024-05-11 14:38:53 -0500

    setup

---
+++ inheritance/exercises/technology/src/main/java/org/launchcode/AbstractEntity.java
@@ -0,0 +1,12 @@
+package org.launchcode;
+
+public abstract class AbstractEntity {
+//    each class has one Id property
+    private int identity;
+
+    // no two objects created from any class may have the same value for Id
+
+    // behavior to assign a unique Id for each class that extends it
+
+    // behavior to access a unique Id for each class that extends it
+}
