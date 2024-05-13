688c8370aa29ac85ab7712c5daec39ff3856ed18
ohcrums <149456208+ohcrums@users.noreply.github.com>
2024-05-13 13:24:35 -0500

    setup

--- inheritance/studio/fun-with-quizzes/src/main/java/org/launchcode/Question.java
+++ inheritance/studio/fun-with-quizzes/src/main/java/org/launchcode/Question.java
@@ -1,4 +1,26 @@
 package org.launchcode;
 
-public class Question {
+public abstract class Question {
+    private String questionText;
+
+    public Question(String questionText){
+        this.questionText = questionText;
+    }
+
+    public String getQuestionText() {
+        return questionText;
+    }
+
+    public void setQuestionText(String questionText) {
+        this.questionText = questionText;
+    }
+
+    public void showQuestion() {
+        System.out.println(getQuestionText());
+    }
+
+    public abstract void showOptions();
+    public abstract boolean checkAnswer(String answer);
+
+
 }
