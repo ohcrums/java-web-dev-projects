54962e88a1234638689b99ecfcde48169f8120d9
ohcrums <149456208+ohcrums@users.noreply.github.com>
2024-05-13 16:50:19 -0500

    done

--- interfaces/exercises/ice-cream-exercises/src/main/java/org/launchcode/ConeComparator.java
+++ interfaces/exercises/ice-cream-exercises/src/main/java/org/launchcode/ConeComparator.java
@@ -6,8 +6,8 @@ public class ConeComparator implements Comparator<Flavor> {
 
     @Override
     public int compare(Flavor f1, Flavor f2){
-        Double df1 = (Double) f1.getCost();
-        Double df2 = (Double) f2.getCost();
+        Double df1 = f1.getCost();
+        Double df2 = f2.getCost();
         return df1.compareTo(df2);
     }
 }
